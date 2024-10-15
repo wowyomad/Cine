1b6de1a4278f97f9dda004ee66f41993ebaea494
wowyomad <vadimsun8837@gmail.com>
2024-10-15 23:00:12 +0300

    Fixed spacing of transform items.

--- Cine Editor/CineEditor/Panels/SceneHierarchyPanel.cpp
+++ Cine Editor/CineEditor/Panels/SceneHierarchyPanel.cpp
@@ -99,52 +99,26 @@ namespace Cine
 		float lineHeight = GImGui->Font->FontSize + GImGui->Style.FramePadding.y * 2.0f;
 		ImVec2 buttonSize = { lineHeight + 3.0f, lineHeight };
 
-		ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.8f, 0.1f, 0.15f, 1.0f));
-		ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0.9f, 0.2f, 0.12f, 1.0f));
-		ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(0.8f, 0.1f, 0.15f, 1.0f));
-		ImGui::PushFont(boldFont);
-		if (ImGui::Button("X", buttonSize))
-		{
-			values.x = resetValue;
-		}
-		ImGui::PopFont();
-		ImGui::PopStyleColor(3);
-		ImGui::SameLine();
-		ImGui::DragFloat("##X", &values.x, 0.1f, 0.0f, 0.0f, "%.2f");
-		ImGui::PopItemWidth();
-		ImGui::SameLine();
+		auto drawControl = [&](const char* label, float& value, const ImVec4& color) {
+			ImGui::PushStyleColor(ImGuiCol_Button, color);
+			ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(color.x + 0.1f, color.y + 0.1f, color.z + 0.1f, 1.0f));
+			ImGui::PushStyleColor(ImGuiCol_ButtonActive, color);
+			ImGui::PushFont(boldFont);
 
-		ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.2f, 0.7f, 0.3f, 1.0f));
-		ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0.3f, 0.8f, 0.3f, 1.0f));
-		ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(0.2f, 0.7f, 0.2f, 1.0f));
-		ImGui::PushFont(boldFont);
-		if (ImGui::Button("Y", buttonSize))
-		{
-			values.y = resetValue;
-		}
-		ImGui::PopFont();
-		ImGui::PopStyleColor(3);
+			if (ImGui::Button(label, buttonSize))
+				value = resetValue;
 
-		ImGui::SameLine();
-		ImGui::DragFloat("##Y", &values.y, 0.1f, 0.0f, 0.0f, "%.2f");
-		ImGui::PopItemWidth();
-		ImGui::SameLine();
+			ImGui::PopFont();
+			ImGui::PopStyleColor(3);
+			ImGui::SameLine();
+			ImGui::DragFloat(("##" + std::string(label)).c_str(), &value, 0.1f, 0.0f, 0.0f, "%.2f");
+			ImGui::PopItemWidth();
+			ImGui::SameLine();
+			};
 
-		ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.1f, 0.25f, 0.8f, 1.0f));
-		ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0.2f, 0.35f, 0.9f, 1.0f));
-		ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(0.1f, 0.25f, 0.8f, 1.0f));
-		ImGui::PushFont(boldFont);
-		if (ImGui::Button("Z", buttonSize))
-		{
-			values.z = resetValue;
-		}
-		ImGui::PopFont();
-		ImGui::PopStyleColor(3);
-
-		ImGui::SameLine();
-		ImGui::DragFloat("##Z", &values.z, 0.1f, 0.0f, 0.0f, "%.2f");
-		ImGui::PopItemWidth();
-		ImGui::SameLine();
+		drawControl("X", values.x, ImVec4{ 0.8f, 0.1f, 0.15f, 1.0f });
+		drawControl("Y", values.y, ImVec4{ 0.2f, 0.7f, 0.3f, 1.0f });
+		drawControl("Z", values.z, ImVec4{ 0.1f, 0.25f, 0.8f, 1.0f });
 
 		ImGui::PopStyleVar();
 
@@ -314,14 +288,16 @@ namespace Cine
 				auto& translation = tc.Translation;
 				DisplayVec3Control("Position", translation);
 
+				ImGui::Spacing();
+
 				auto rotation = glm::degrees(tc.Rotation);
 				DisplayVec3Control("Rotation", rotation);
 				tc.Rotation = glm::radians(rotation);
 
+				ImGui::Spacing();
+
 				auto& scale = tc.Scale;
 				DisplayVec3Control("Scale", scale, 1.0f);
-
-
 			});
 	}
 
