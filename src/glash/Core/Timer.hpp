68272a84442053ffc8eaebacfa12e74d63a8fecb
wowyomad <vadimsun8837@gmail.com>
2024-10-18 00:08:11 +0300

    Added Timer class.

--- src/glash/Core/Timer.hpp
+++ src/glash/Core/Timer.hpp
@@ -6,11 +6,11 @@
 namespace Cine
 {
 	template <class Fn>
-	class Timer
+	class BenchmarkTimer
 	{
 
 	public:
-		Timer(const char* name, Fn&& resultCallback)
+		BenchmarkTimer(const char* name, Fn&& resultCallback)
 			: m_Name(name), m_ResultCallback(resultCallback)
 		{
 			Reset();
@@ -39,7 +39,7 @@ namespace Cine
 			m_ResultCallback({ m_Name, elapsed });
 		}
 
-		~Timer()
+		~BenchmarkTimer()
 		{
 			if (!m_Stopped)
 			{
@@ -53,5 +53,53 @@ namespace Cine
 		const char* m_Name;
 		std::chrono::time_point<std::chrono::high_resolution_clock> m_Start;
 
+	};
+
+	class Timer
+	{
+	public:
+		Timer() : m_Started(false), m_ElapsedTime(0.0f) {}
+
+		void Start()
+		{
+			m_StartTimePoint = std::chrono::steady_clock::now();
+			m_Started = true;
+			m_ElapsedTime = 0.0f;
+		}
+
+		void OnUpdate(float ts)
+		{
+			if (m_Started)
+			{
+				m_ElapsedTime += ts; 
+			}
+		}
+
+		std::chrono::steady_clock::time_point GetTimeStarted() const
+		{
+			return m_StartTimePoint;
+		}
+
+		float GetElapsed() const
+		{
+			return m_ElapsedTime;
+		}
+
+		// Stops the timer
+		void Stop()
+		{
+			m_Started = false;
+		}
+
+		// Checks if the timer is currently running
+		bool IsRunning() const
+		{
+			return m_Started;
+		}
+
+	private:
+		bool m_Started;
+		float m_ElapsedTime; // Accumulated elapsed time in seconds
+		std::chrono::steady_clock::time_point m_StartTimePoint;
 	};
 }
